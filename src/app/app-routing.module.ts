8fb71bd4f715536c96ebe11a685dfc174a10c839
Azhar Uddin <akhan@gainsight.com>
2020-06-15 05:13:22 +0530

    Added Menu missing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ButtonComponent } from './button/button.component';
 import { LayoutComponent } from './layout/layout.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 const routes: Routes = [{
@@ -10,6 +11,9 @@ const routes: Routes = [{
 },{
   path: "button",
   component: ButtonComponent
+},{
+  path: "menu",
+  component: MenuComponent
 }];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
 import { LayoutComponent } from './layout/layout.component';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { MenuComponent } from './menu/menu.component';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    LayoutComponent
+    LayoutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    NzLayoutModule
+    NzBreadCrumbModule,
+    NzLayoutModule,
+    ScrollingModule,
+    NzMenuModule,
+    NzDropDownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
