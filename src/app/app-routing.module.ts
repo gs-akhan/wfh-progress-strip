4ad1b72cbb70e6d46d38c34bc251fad6e3d0dd72
Azhar Uddin <akhan@gainsight.com>
2020-06-26 06:05:31 +0530

    Added Date picker to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,26 +6,30 @@ import { MenuComponent } from './menu/menu.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
 import { StepsComponent } from './steps/steps.component';
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
 
 
 const routes: Routes = [{
   path: "layout",
   component: LayoutComponent
-},{
+}, {
   path: "button",
   component: ButtonComponent
-},{
+}, {
   path: "menu",
   component: MenuComponent
-},{
+}, {
   path: "dropdown",
   component: DropdownComponent
-},{
+}, {
   path: "steps",
   component: StepsComponent
-},{
+}, {
   path: "autocomplete",
   component: AutocompleteComponent
+}, {
+  path: "datepicker",
+  component: DatepickerComponent
 }];
 
 @NgModule({
