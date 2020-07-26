955dbe44b83bc218241136e11b48295db2e5cd3c
Azhar Uddin <akhan@gainsight.com>
2020-07-27 03:27:21 +0530

    Moved routes to another file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,52 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ButtonComponent } from './button/button.component';
-import { LayoutComponent } from './layout/layout.component';
-import { MenuComponent } from './menu/menu.component';
-import { DropdownComponent } from './dropdown/dropdown.component';
-import { StepsComponent } from './steps/steps.component';
-import { AutocompleteComponent } from './autocomplete/autocomplete.component';
-import { DatepickerComponent } from './datepicker/datepicker.component';
-import { CardComponent } from './card/card.component';
-import { ListComponent } from './list/list.component';
-import { NotificationComponent } from './notification/notification.component';
-import { ModalComponent } from './modal/modal.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './routes';
 
 
-const routes: Routes = [{
-  path: "layout",
-  component: LayoutComponent
-}, {
-  path: "button",
-  component: ButtonComponent
-}, {
-  path: "menu",
-  component: MenuComponent
-}, {
-  path: "dropdown",
-  component: DropdownComponent
-}, {
-  path: "steps",
-  component: StepsComponent
-}, {
-  path: "autocomplete",
-  component: AutocompleteComponent
-}, {
-  path: "datepicker",
-  component: DatepickerComponent
-}, {
-  path: "card",
-  component: CardComponent
-},{
-  path: "list",
-  component: ListComponent
-}, {
-  path: "notification",
-  component: NotificationComponent
-}, {
-  path : "modal",
-  component : ModalComponent
-}];
 //Added a comment
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/routes.ts
@@ -0,0 +1,46 @@
+import { Routes } from '@angular/router';
+import { ButtonComponent } from './button/button.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MenuComponent } from './menu/menu.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { StepsComponent } from './steps/steps.component';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { CardComponent } from './card/card.component';
+import { ListComponent } from './list/list.component';
+import { NotificationComponent } from './notification/notification.component';
+import { ModalComponent } from './modal/modal.component';
+export const routes: Routes = [{
+  path: "layout",
+  component: LayoutComponent
+}, {
+  path: "button",
+  component: ButtonComponent
+}, {
+  path: "menu",
+  component: MenuComponent
+}, {
+  path: "dropdown",
+  component: DropdownComponent
+}, {
+  path: "steps",
+  component: StepsComponent
+}, {
+  path: "autocomplete",
+  component: AutocompleteComponent
+}, {
+  path: "datepicker",
+  component: DatepickerComponent
+}, {
+  path: "card",
+  component: CardComponent
+}, {
+  path: "list",
+  component: ListComponent
+}, {
+  path: "notification",
+  component: NotificationComponent
+}, {
+  path: "modal",
+  component: ModalComponent
+}];
