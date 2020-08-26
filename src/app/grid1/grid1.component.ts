094616f7a4ff2567d971d16fc58a62a9eaf0091e
Azhar Uddin <akhan@gainsight.com>
2020-08-26 15:29:18 +0530

    Added grid 1 to routes header

--- src/app/grid1/grid1.component.ts
+++ src/app/grid1/grid1.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Grid1Component implements OnInit {
 
+  languages = ["Rust", "Go Lang", "Java", "ErLang", "Perl"];
   constructor() { }
 
   ngOnInit(): void {
