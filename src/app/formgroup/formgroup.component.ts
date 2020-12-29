74dddc6cc8b4b8785ad61ef8e1462a22a65c27df
Azhar Uddin <akhan@gainsight.com>
2020-12-30 04:55:07 +0530

    Added reactive form- a bit of complex form

--- src/app/formgroup/formgroup.component.ts
+++ src/app/formgroup/formgroup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormArray, FormGroup } from '@angular/forms';
+import { FormBuilder } from "@angular/forms";
 @Component({
   selector: 'app-formgroup',
   templateUrl: './formgroup.component.html',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormgroupComponent implements OnInit {
 
-  constructor() { }
+
+  volunteerForm: FormGroup;
+  locations = ["Hyderabad", "Bangalore", "Delhi", "Mumbai"];
+  constructor(private fb: FormBuilder) {
+    this.initForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  addPreference() {
+    (this.volunteerForm.get("preferences") as FormArray).push(this.fb.control(''));
+  }
+
+  removeEmail(index) {
+    (this.volunteerForm.get("preferences") as FormArray).removeAt(index);
+  }
+  initForm() {
+    this.volunteerForm = this.fb.group({
+      name: "",
+      phoneNumber: "",
+      location: "",
+      animals: this.fb.group({
+        dogs: false,
+        cats: false,
+        reptiles: false
+      }),
+      preferences: this.fb.array([this.fb.control('')])
+    })
+  }
+
+  submitForm() {
+    console.log(this.volunteerForm.value);
+  }
+  get preferences(): FormArray {
+    return this.volunteerForm.get("preferences") as FormArray
   }
 
 }
